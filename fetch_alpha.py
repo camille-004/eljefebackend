eebdd77c39e6d372dafc090d818cf265a0835b52
camille-004 <dunningcamille@gmail.com>
2021-09-08 14:53:52 -0700

    Added class to fetch data

--- fetch_alpha.py
+++ fetch_alpha.py
@@ -21,58 +21,64 @@ TOKEN = os.getenv('API_KEY')
 START_DATE = dateutil.parser.parse('2016-01-01')
 
 
-def get_stock_data(tickers=TICKERS, start_date=START_DATE, token=TOKEN):
-    """
-    A function to get financial data for a given list of tickers and save to a
-    JSON file
+class AlphaAPIFetch:
+    """Class to fetch historical stock data after a certain start date and
+    fundamental data for the given tickers"""
 
-    :param tickers: input tickers
-    :param start_date: keep data after this date
-    :param token: Alpha Vantage API key
-    """
-    n_requests = 0
-    for ticker in tqdm(tickers):
-        n_requests += 1
-        if n_requests > CALLS_PER_MINUTE:
-            time.sleep(TIME_SLEEP)
-            n_requests = 0
-        ts = TimeSeries(key=token, output_format='pandas')
-        stocks = ts.get_daily_adjusted(ticker, outputsize='full')[0]
-        stocks.columns = stocks.columns.map(lambda x: x.split('.')[1].strip())
-        stocks = stocks[stocks.index > start_date]
-        stocks.reset_index(level=0, inplace=True)
-        stocks['date'] = stocks['date'].dt.strftime('%Y-%m-%d')
-        stocks.to_json(
-            f'{DATA_DIR}{ticker}_financial.json',
-            orient='records',
-            indent=4,
-            date_unit='ns'
-        )
+    def __init__(self, tickers=TICKERS, start_date=START_DATE, token=TOKEN):
+        self.tickers = tickers
+        self.start_date = start_date
+        self.token = token
 
+        self.max_calls = CALLS_PER_MINUTE
+        self.time_sleep = TIME_SLEEP
+        self.n_requests = 0
 
-def get_fundamentals(tickers=TICKERS, token=TOKEN):
-    """
-    A function to get fundamental company data for a given list of tickers and
-    save to a JSON file
+        self.get_stock_data()
+        self.get_fundamentals()
 
-    :param tickers: input tickers
-    :param token: Alpha Vantage API key
-    """
-    fundamentals = []
-    n_requests = 0
-    for ticker in tqdm(tickers):
-        n_requests += 1
-        if n_requests > CALLS_PER_MINUTE:
-            time.sleep(TIME_SLEEP)
-            n_requests = 0
-        url = f'https://www.alphavantage.co/query?function=OVERVIEW' \
-              f'&symbol={ticker}&apikey={token}'
-        r = requests.get(url)
-        fundamentals.append(r.json())
-    with open(f'{DATA_DIR}fundamentals.json', 'w') as f:
-        json.dump(fundamentals, f, indent=4)
+    def get_stock_data(self):
+        """
+        A method to get financial data for a given list of tickers and save
+        to a JSON file
+        """
+        for ticker in tqdm(self.tickers):
+            self.n_requests += 1
+            if self.n_requests > self.max_calls - 1:
+                time.sleep(TIME_SLEEP)
+                self.n_requests = 0
+            ts = TimeSeries(key=self.token, output_format='pandas')
+            stocks = ts.get_daily_adjusted(ticker, outputsize='full')[0]
+            stocks.columns = stocks.columns.map(
+                lambda x: x.split('.')[1].strip())
+            stocks = stocks[stocks.index > self.start_date]
+            stocks.reset_index(level=0, inplace=True)
+            stocks['date'] = stocks['date'].dt.strftime('%Y-%m-%d')
+            stocks.to_json(
+                f'{DATA_DIR}{ticker}_financial.json',
+                orient='records',
+                indent=4,
+                date_unit='ns'
+            )
+
+    def get_fundamentals(self):
+        """
+        A method to get fundamental company data for a given list of tickers
+        and save to a JSON file
+        """
+        fundamentals = []
+        for ticker in tqdm(self.tickers):
+            self.n_requests += 1
+            if self.n_requests > self.max_calls - 1:
+                time.sleep(self.time_sleep)
+                self.n_requests = 0
+            url = f'https://www.alphavantage.co/query?function=OVERVIEW' \
+                  f'&symbol={ticker}&apikey={self.token}'
+            r = requests.get(url)
+            fundamentals.append(r.json())
+        with open(f'{DATA_DIR}fundamentals.json', 'w') as f:
+            json.dump(fundamentals, f, indent=4)
 
 
 if __name__ == '__main__':
-    get_stock_data()
-    get_fundamentals()
+    AlphaAPIFetch()
